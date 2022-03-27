b7996eb6949aa017668ca20e66ae3ab3b59cdeb8
jennj <jennjenn93@gmail.com>
2022-03-27 13:32:37 -0500

    created html and page-template.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+//console.log(profileDataArgs);
 
 //Notice the lack of parentheses around the 'profileDataArr' parameter?
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
     //this...
-    for (let i=0; i < profileDataArr.length; i+=1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('======================');
+  //  for (let i=0; i < profileDataArr.length; i+=1) {
+    //console.log(profileDataArr[i]);
+    //}
+    //console.log('======================');
 
     //Is the same as this...
-    profileDataArr.forEach(profileItem =>  console.log(profileItem));
-};
+    //profileDataArr.forEach(profileItem =>  console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
