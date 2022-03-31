a7a98c9f2d3cb0be3f5c181a26f58c52093c9f8a
jennj <jennjenn93@gmail.com>
2022-03-30 23:12:34 -0500

    creating more html based on data pulled

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -136,15 +140,18 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+  fs.writeFile('./index.html', pageHTML, err => {
+      if(err) throw  new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+  });
 });
 
 
 
 
-//const fs = require('fs');
 
-//const generatePage = require('./src/page-template.js');
 
 //const pageHTML = generatePage(name, github);
 
